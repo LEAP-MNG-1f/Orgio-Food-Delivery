3f3f4a89ab6501a7f4498e6ae784a2edd64c0138
Бат-Оргил Даваасүрэн <24LP9786@Users-iMac.local>
2024-11-18 14:05:09 +0800

    last

---
+++ frontend/src/app/_components/headers/Headers.tsx
@@ -0,0 +1,11 @@
+import { PineconeLogo } from "../svg/PineconeLogo";
+
+export const Headers = () => {
+  return (
+    <div className="w-full h-auto">
+      <div className="max-w-[1210px] flex justify-center items-center">
+        <PineconeLogo />
+      </div>
+    </div>
+  );
+};
